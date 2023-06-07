a5aa798300774cd3d9f67a12b6adb051cf01b990
priyanshi1811d <124236582+priyanshi1811d@users.noreply.github.com>
2023-06-07 17:19:06 +0530

    Add files via upload

---
+++ timingset.js
@@ -0,0 +1,11 @@
+console.log("Statement 01");
+console.log("Statement 02");
+console.log("Statement 03");
+
+setTimeout(() => {
+    console.log("Executing setImmediate");
+}, 0);
+
+console.log("Statement 04");
+console.log("Statement 05");
+console.log("Statement 06");
