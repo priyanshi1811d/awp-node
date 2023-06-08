50a32e8fb90cad3fd27c5f50836cb10f28a83812
priyanshi1811d <124236582+priyanshi1811d@users.noreply.github.com>
2023-06-08 15:54:06 +0530

    Add files via upload

---
+++ coki_sess.js
@@ -0,0 +1,23 @@
+var express=require('express');
+var app=express();
+var cookieparser=require('cookie-parser');
+var session=require('express-session');
+app.use(cookieparser());
+app.use(session({
+    secret:'secret',
+    cookie:{
+        maxAge:10000
+    }
+}));
+app.get('/',function(req,res){
+    if(req.session.count==null){
+        req.session.count=1;
+    }else{
+        req.session.count+=1;
+    }
+    res.send((JSON.stringify(req.session.count))+"<br>"+JSON.stringify(req.sessionID));
+})
+app.listen(3000,()=>
+{
+    console.log('server listening to localhost:3000')
+});
